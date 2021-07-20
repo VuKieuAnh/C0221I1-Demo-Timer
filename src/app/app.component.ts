4b83d02520e68ec65e13cf78158190419152f0b7
kieuanh <vukieuanh.hnue@gmail.com>
2021-07-20 09:30:15 +0700

    add count down

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demoC02I';
+
+  isCountDown='Chua ket thuc';
+
+  finnishCha(){
+    this.isCountDown = 'Ket thuc';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PipeDemoComponent } from './pipe-demo/pipe-demo.component';
+import { RatingBarComponent } from './rating-bar/rating-bar.component';
+import { TimeCountDownComponent } from './time-count-down/time-count-down.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipeDemoComponent,
+    RatingBarComponent,
+    TimeCountDownComponent
   ],
   imports: [
     BrowserModule
